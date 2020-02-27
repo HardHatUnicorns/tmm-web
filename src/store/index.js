04705b5c71a66ca371d65fe9373cf877fa4f71f3
Mateusz Ozog <mateusz2345678@gmail.com>
2020-02-27 09:53:40 +0100

    :zap: Initial commit

--- src/store/index.js
+++ src/store/index.js
@@ -4,19 +4,19 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-	state: {
-		appName: 'TMM',
-		appVersion: '0.6.1221'
-	},
-	getters: {
-		getAppName(state) {
-			return state.appName;
-		},
-		getAppVersion(state) {
-			return state.appVersion;
-		}
-	},
-	mutations: {},
-	actions: {},
-	modules: {}
+    state: {
+        appName: 'TMM',
+        appVersion: '0.1.0'
+    },
+    getters: {
+        getAppName(state) {
+            return state.appName;
+        },
+        getAppVersion(state) {
+            return state.appVersion;
+        }
+    },
+    mutations: {},
+    actions: {},
+    modules: {}
 });
