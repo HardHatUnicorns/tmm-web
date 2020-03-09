175a1e82cd8b29c537d485b131e9a460b555046a
Mateusz Ożóg <mateusz2345678@gmail.com>
2020-03-09 20:18:48 +0100

    :art: Register / Login form + Inputs validation styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 
+// Scripts 
+import { Scripts } from '../scripts';
+
 // Ignore JQuery
 declare var $: any;
 
@@ -12,9 +15,18 @@ declare var $: any;
 })
 export class AppComponent {
 	constructor(private router: Router, private http: HttpClient) {
+		// Call every time on loading new route
 		router.events.subscribe(e => {
+			// Import scripts
+			const sc = new Scripts();
 			// Hide mobile bootstrap menu
 			$('.collapse').collapse('hide');
+			// Toggle all tooltips
+			$('[data-toggle="tooltip"]').tooltip('hide');
+			$(function () {
+				$('[data-toggle="tooltip"]').tooltip();
+				sc.loadInputs();
+			});
 		});
 	}
 }
