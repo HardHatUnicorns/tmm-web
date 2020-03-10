b6508496218dbb4a3932198fc79c5f0e07d25198
Mateusz Ożóg <mateusz2345678@gmail.com>
2020-03-11 00:11:39 +0100

    :pencil: Register validation from form to react

--- src/app/views/auth/register/register.component.ts
+++ src/app/views/auth/register/register.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import { AlertService } from 'src/app/services/alert.service';
 
 @Component({
 	selector: 'app-auth-register',
@@ -11,31 +11,57 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
 export class RegisterComponent implements OnInit {
 
 	registerForm: FormGroup;
-	inProgress: boolean = false;
+	submited: boolean = false;
 
-	constructor(private fb: FormBuilder, private authService: AuthService) { }
+	constructor(private fb: FormBuilder, private authService: AuthService, private alertService: AlertService) { }
 
 	ngOnInit(): void {
 		this.registerForm = this.fb.group({
-			email: '',
-			login: '',
-			plainPassword: '',
-			plainPasswordConfirm: ''
+			email: ['', [
+				Validators.required,
+				Validators.email
+			]],
+			login: ['', [
+				Validators.required,
+				Validators.minLength(6),
+				Validators.maxLength(32)
+			]],
+			plainPassword: ['', [
+				Validators.required,
+				Validators.minLength(8),
+				Validators.maxLength(64),
+				Validators.pattern(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/)
+			]],
+			plainPasswordConfirm: ['', [
+				Validators.required,
+				Validators.minLength(8),
+				Validators.maxLength(64),
+				Validators.pattern(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/)
+			]]
+		});
+
+		// Passwords must be identical
+		this.registerForm.controls['plainPasswordConfirm'].valueChanges.subscribe(plainPasswordConfirm => {
+			if (plainPasswordConfirm !== this.registerForm.controls['plainPassword'].value) {
+				this.registerForm.controls['plainPasswordConfirm'].setErrors({ passwordMath: 'no' });
+			} else {
+				this.registerForm.controls['plainPasswordConfirm'].setErrors(null);
+			}
 		})
 	}
 
 	register(): void {
-		if (!this.inProgress) {
-			this.inProgress = true;
+		if (!this.submited) {
+			this.submited = true;
 			this.authService.registerUser(this.registerForm.value)
 				.subscribe(
 					res => {
-						// Success
-						this.inProgress = false;
+						this.alertService.emitChange({ visible: true, type: 'success', message: 'Registered' });
+						this.submited = false;
 					},
 					error => {
-						// Error
-						this.inProgress = false;
+						this.alertService.emitChange({ visible: true, type: 'danger', message: 'ERROR' });
+						this.submited = false;
 					}
 				);
 		}
